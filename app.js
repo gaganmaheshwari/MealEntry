7672606d8b2e9c6512a5e810883000cc2a1bd30b
Natalie Romano <brandnew03@gmail.com>
2016-09-24 16:16:50 -0700

    fixed heroku port

--- app.js
+++ app.js
@@ -59,5 +59,5 @@ app.post("/meal", function(req, res) {
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("Server running on port 3000");
