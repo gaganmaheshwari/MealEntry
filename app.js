34571ccc27f73a8420ca397d1c2468cc0feb6480
Natalie Romano <brandnew03@gmail.com>
2016-09-24 18:53:05 -0700

    changed endpoint syntax

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 //declaring express on the server.js file
 var express = require("express");
+var bodyParser = require('body-parser');
+
 var app = express();
 
 var mongoose = require('mongoose');
+
+var jsonParser = bodyParser.json();
 
 mongoose.connect( 'mongodb://togetherly:togetherly2016@ds046549.mlab.com:46549/mealtally', function (err) { //TODO: config
  if (err) console.log('Error: Failed to connect to mongoose!', err);
@@ -28,6 +32,10 @@ var mealSchema = mongoose.Schema({
 
 var meal = mongoose.model('meal', mealSchema);﻿
 
+
+app.listen(process.env.PORT || 3000);
+console.log("Server running on port 3000");
+
 app.get("/meal", function(req, res) {
   meal.find({}, function(err, result) {
     if(err) res.status(500).json(err);
@@ -35,29 +43,11 @@ app.get("/meal", function(req, res) {
   });
 });
 
-app.post("/meal", function(req, res) {
-  meal.insert({
-    "date": req.body.date,
-    "siteName": req.body.siteName,
-    "meal": {
-      "type": req.body.meal.type,
-      "vendorReceived": req.body.meal.vendorReceived,
-      "carryOver": req.body.meal.carryOver,
-      "consumed": {
-        "child": req.body.meal.consumed.child,
-        "adult": req.body.meal.consumed.adult,
-        "volunteer": req.body.meal.consumed.volunteer
-      },
-      "damaged": req.body.meal.damaged,
-      "wasted": req.body.meal.wasted
-    }
-  }, function(err, result) {
-    if(err) res.status(500).json(err);
-    else res.status(200).json(result);
+app.post('/meal', function(req, res) {
+  new meal(req.body).save(function(err, result){
+            if(err) res.status(500).json(err);
+            else res.status(200).json(result);
   });
 });
 
 app.use(express.static(__dirname + "/public"));
-
-app.listen(process.env.PORT || 3000);
-console.log("Server running on port 3000");
